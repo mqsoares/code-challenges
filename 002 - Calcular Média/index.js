0a23e22cba568cd29258f5907cc9463b183c42ac
mqsoares <mqseraos@gmail.com>
2022-08-14 23:16:45 -0300

    Calcular Média

---
+++ 002 - Calcular Média/index.js
@@ -0,0 +1,19 @@
+function calcMedia() {
+    
+    let total = 0;
+    let qtde = arguments.length;
+    
+    for (let i = 0; i < qtde ; i++) {
+        if (typeof arguments[i] !== 'number') {
+            throw Error(' Error - somente números');
+        } 
+        total += arguments[i];
+    } 
+
+    return (total / qtde) || 0
+}
+
+let media = calcMedia()
+// let media = calcMedia(2,6)
+// let media = calcMedia(2,"6")
+console.log(media)
